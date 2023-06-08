061c3615777e0aeca9522eda3a2de2e1a6856179
Patrick Stokes <Prstokes37@gmail.com>
2023-06-07 20:51:59 -0500

    final

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const newPlayerFormContainer = document.getElementById("new-player-form");
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = "2302-ACC-CT-WEB-PT-A";
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-A/players`;
 
 /**
  * It fetches all players from the API and returns them
@@ -27,7 +27,6 @@ const fetchSinglePlayer = async (playerId) => {
         const player = data.data.player;
 
         playerContainer.innerHTML = "";
-        newPlayerFormContainer.innerHTML = "";
         const playerDetailsElement = document.createElement("div");
         playerDetailsElement.classList.add("player-details");
         playerDetailsElement.innerHTML = `
@@ -52,6 +51,21 @@ const fetchSinglePlayer = async (playerId) => {
     }
 };
 
+// const addNewPlayer = async (playerObj) => {
+//   try {
+//     const response = await fetch(APIURL, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(playerObj)
+//     });
+//     const newDog = await response.json();
+//     console.log(newDog);
+//   } catch (err) {
+//     console.error("Oops, something went wrong with adding that player!", err);
+//   }
+// };
 const addNewPlayer = async (playerObj) => {
     try {
         const response = await fetch(APIURL, {
@@ -59,10 +73,19 @@ const addNewPlayer = async (playerObj) => {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(playerObj)
+            body: JSON.stringify({
+                name: playerObj.name,
+                breed: playerObj.breed,
+                status: playerObj.status,
+                imageURL: playerObj.imageURL,
+                createdAt: playerObj.createdAt,
+                updtedAt: playerObj.updtedAt,
+                teamId: playerObj.teamId,
+                cohortId: playerObj.cohortId
+            })
         });
-        const newDog = await response.json();
-        return newDog;
+        const result = await response.json();
+        console.log(result);
     } catch (err) {
         console.error("Oops, something went wrong with adding that player!", err);
     }
@@ -74,8 +97,8 @@ const removePlayer = async (playerId) => {
             method: "DELETE"
         });
         const player = await response.json();
+        console.log(player);
         fetchAllPlayers();
-
         window.location.reload();
     } catch (err) {
         console.error(
@@ -147,17 +170,19 @@ const renderAllPlayers = async (playerList) => {
 const renderNewPlayerForm = () => {
     try {
         const formHtml = `
-        <form class="players-container">
-          <label for="name">Name</label>
-          <input id="name" name="name" placeholder="Banjo">
-          <label for="breed">Breed</label>
-          <input id="breed" name="breed" placeholder="Lab">
-          <label for="status">Status</label>
-          <input id="status" name="status" placeholder="feild">
-          <label for="img">IMGURL</label>
-          <input id="img" name="img" placeholder="img">
-          <button type="submit">Submit</button>
-          </form>`;
+      <form class="players-container">
+        <h2> Add Dog </h2>
+        <img id ="dogimg" src= "https://media.istockphoto.com/id/639255368/vector/vector-black-silhouette-of-a-dog.jpg?s=612x612&w=0&k=20&c=LxmhSbJvDDhjkC56P1P9rW-SdVXejpTZroOd_6WlYDk=" alt="dog">
+        <label for="name">Name:</label>
+        <input id="name" name="name" placeholder="Banjo"><br>
+        <label for="breed">Breed:</label>
+        <input id="breed" name="breed" placeholder="Chocolate Labrador"><br>
+        <label for="status">Status:</label>
+        <input id="status" name="status" placeholder="bench"><br>
+        <label for="img">Image:</label>
+        <input id="img" name="img" placeholder="img-URL"><br>
+        <button class="btn" type="submit">Submit</button>
+        </form>`;
         newPlayerFormContainer.innerHTML = formHtml;
 
         newPlayerFormContainer.addEventListener("submit", async (event) => {
